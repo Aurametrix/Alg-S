448720751651d8dbac3de5e715945307369b9e08
Aurametrix <irene@aurametrix.com>
2015-10-21 22:51:09 -0700

    99 bottles of beer - trivial solution

---
+++ PROBLEMS/99bottles.scala
@@ -0,0 +1,7 @@
+99 to 1 by -1 foreach { n =>
+  println(
+    f"$n%d bottles of beer on the wall\n" +
+      f"$n%d bottles of beer\n" +
+      f"Take one down, pass it around\n" +
+      f"${n - 1}%d bottles of beer on the wall\n")
+}
