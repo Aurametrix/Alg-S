264e7cf9ab16ebe0c045c658b453422f7b137158
Aurametrix <irene@aurametrix.com>
2017-01-20 12:09:04 -0800

    speeding up by reversing the frequencies list before processing it

--- SORT/countingsort.scala
+++ SORT/countingsort.scala
@@ -2,6 +2,6 @@ def countSort(input: List[Int], min: Int, max: Int): List[Int] =
   input.foldLeft(Array.fill(max - min + 1)(0)) { (arr, n) => 
     arr(n - min) += 1
     arr
-  }.zipWithIndex.foldLeft(List[Int]()) {
+  }.zipWithIndex.reverse.foldLeft(List[Int]()) {
     case (lst, (cnt, ndx)) => List.fill(cnt)(ndx + min) ::: lst
-  }.reverse
+  }
